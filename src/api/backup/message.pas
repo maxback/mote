df6a5589ada64deaa8a60fdbce7f5a30bffaf449
Max Back <max.back@gmail.com>
2020-10-20 14:01:40 -0300

    iniciando projeto com unit main e de mensagem

---
+++ src/api/backup/message.pas
@@ -0,0 +1,13 @@
+unit message;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
---
+++ src/api/message.pas
@@ -0,0 +1,19 @@
+unit message;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TMoteMessage = class
+    FsTopic: string;
+    FsPayload: string;
+  end;
+
+implementation
+
+end.
+
